0f3a3491348c1ce464442ddbbaec010aa8380256
pk393256 <pk393256@gmail.com>
2022-07-31 17:34:56 +0530

    single movie page modified

--- imdb_frontend/src/searchBar/singleSeaech.jsx
+++ imdb_frontend/src/searchBar/singleSeaech.jsx
@@ -7,26 +7,30 @@ import action from '../image/action.png';
 import singleImdb from '../image/singleImdb.png';
 import netflix from '../image/netflix.png';
 import meta from '../image/meta.png';
+import releatedNews from '../image/Related news.png';
+import first from '../image/first.png';
+import second from '../image/second.png';
+import contribute from '../image/contribute.png';
 import { Box, Image, Flex, Text, Link, Divider, Button } from '@chakra-ui/react';
 import { useState } from "react";
 
 export function SingleMovie() {
-    let [data,setData]=useState({});
-    let {id} = useParams();
-    console.log(id)
-    function getData(){
-        fetch(`http//:localhost3000/allMovies/${id}`)
-        .then((res)=>res)
-        .then((e)=>setData(e))
-        .catch((err)=>console.log(err))
-    }
-
-    // let data = {
-    //     fullTitle: "Spider Man:Far from home",
-    //     year: "2021",
-    //     imDbRating: "4.1",
-    //     image: "https://via.placeholder.com/300.png"
+    // let [data,setData]=useState({});
+    // let {id} = useParams();
+    // console.log(id)
+    // function getData(){
+    //     fetch(`http//:localhost3000/allMovies/${id}`)
+    //     .then((res)=>res)
+    //     .then((e)=>setData(e))
+    //     .catch((err)=>console.log(err))
     // }
+
+    let data = {
+        fullTitle: "Spider Man:Far from home",
+        year: "2021",
+        imDbRating: "4.1",
+        image: "https://via.placeholder.com/300.png"
+    }
     return (
         <>
             <Box background={'black'}>
@@ -99,28 +103,51 @@ export function SingleMovie() {
 
                     </Box>
                     <Box w={"400px"}>
-                        <Box h={'15%'}  textAlign='left' backgroundColor={'#f5c518'} borderRadius='5px'>
+                        <Box h={'15%'} textAlign='left' backgroundColor={'#f5c518'} borderRadius='5px'>
                             <Link >
                                 <Image mr={"60%"} w={'150px'} pt={'8px'} src={netflix} /><Text></Text>
                             </Link>
                         </Box>
-                        <Box mt={'10px'} h={'15%'}  textAlign='left' backgroundColor={'#313131'} borderRadius='5px'>
+                        <Box mt={'10px'} h={'15%'} textAlign='left' backgroundColor={'#313131'} borderRadius='5px'>
                             <Link >
                                 <Text ml={"15px"} pt={'10px'}>≡ More Watch Option</Text>
                             </Link>
                         </Box>
-                        <Box mt={'10px'} h={'15%'}  textAlign='left' backgroundColor={'#313131'} borderRadius='5px'>
+                        <Box mt={'10px'} h={'15%'} textAlign='left' backgroundColor={'#313131'} borderRadius='5px'>
                             <Link >
                                 <Text ml={"15px"} pt={'10px'}>+ Add to Watchlist</Text>
                             </Link>
-                            
+
                         </Box>
                         <Link>
-                        <Image mt={'13px'} width={'300px'} src={meta}/>
+                            <Image mt={'13px'} width={'300px'} src={meta} />
                         </Link>
                     </Box>
                 </Flex>
             </Box>
+            <Link >
+                <Image mt={'50px'} ml={'7%'} w={'250px'} src={releatedNews} />
+            </Link>
+            <Flex mt={'30px'} ml={'7%'} gap={'40px'}>
+                <Link>
+                    <Image w={'400px'} src={first} />
+                </Link>
+                <Link>
+                    <Image w={'400px'} src={second} />
+                </Link>
+            </Flex>
+            <Box ml={'7%'} mt='30px'>
+                <Image w={'350px'} src={contribute}/>
+                <Divider mt={'15px'} orientation="horizontal" />
+                <Link>
+                <Text fontSize={'20px'} mt={'15px'} ml={'10px'}><b>IMDb Answers: Help fill gaps in our data</b></Text>
+                </Link>
+                <Divider mt={'15px'} orientation="horizontal" />
+                <Link>
+                <Text fontSize={'20px'} mt={'15px'} ml={'10px'}><b>Learn more about contributing</b></Text>
+                </Link>
+                <Divider mt={'15px'} orientation="horizontal" />
+            </Box>
         </>
     )
-}
+} 
