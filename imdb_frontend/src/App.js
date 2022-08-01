631d428115244f9774f834a5a66cec85b0430c2a
PRINCE KUMAR <pk393256@gmail.com>
2022-08-01 20:51:07 +0530

    added singleMoviePage Route

--- imdb_frontend/src/App.js
+++ imdb_frontend/src/App.js
@@ -27,6 +27,7 @@ function App() {
       <Route path='/signup' element={<SignUp/>} ></Route>
       <Route path='/createaccount' element={<Createaccount/>} ></Route>
       <Route path='/login' element={<Login/>} ></Route>
+      <Route path='/:id' element={<SingleMovie />} />
       </Routes>
 
 
